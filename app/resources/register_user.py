e1bdf54b40f6c5e432343c94ceb2d555666af854
Devadathan M B <devadathanmb@gmail.com>
2023-05-03 21:44:58 +0530

    Calculate calorie requirement and store it to collection

--- app/resources/register_user.py
+++ app/resources/register_user.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from flask_restful import Resource, reqparse
 from db import users
 from email_validator import validate_email, EmailNotValidError
 from werkzeug.security import generate_password_hash
+
 
 class UserRegistration(Resource):
     def post(self):
@@ -22,6 +24,7 @@ class UserRegistration(Resource):
         parser.add_argument(
             "dob", type=str, help="Date of birth cannot be empty", required=True
         )
+
         parser.add_argument("weight", type=int, required=True)
         parser.add_argument("height", type=int, required=True)
         parser.add_argument(
@@ -29,6 +32,9 @@ class UserRegistration(Resource):
             type=str,
             choices=("male", "female"),
             required=True,
+        )
+        parser.add_argument(
+            "activity_factor", type=int, choices=(1, 1.2, 1.4, 1.6, 1.8), required=True
         )
         args = parser.parse_args()
         try:
@@ -43,6 +49,25 @@ class UserRegistration(Resource):
         # Hash password
         hashed_password = generate_password_hash(args["password"], method="sha256")
 
+        # Calculate age
+        age = datetime.now().year - int(args["dob"].split("/")[2])
+        if age <= 0:
+            return {"message": "Invalid date of birth"}, 400
+
+        # Calculate required calories
+        required_calories = 0
+        if args["gender"] == "male":
+            required_calories = (
+                655.1
+                + ((9.563 * args["weight"]) + (1.850 * args["height"]) - (4.676 * age))
+            ) * args["activity_factor"]
+        elif args["gender"] == "female":
+            required_calories = (
+                66.47
+                + ((13.75 * args["weight"]) + (5.003 * args["height"]) - (6.755 * age))
+                * args["activity_factor"]
+            )
+
         # Insert user into database
         users.insert_one(
             {
@@ -54,6 +79,8 @@ class UserRegistration(Resource):
                 "weight": args["weight"],
                 "height": args["height"],
                 "gender": args["gender"],
+                "activity_factor": args["activity_factor"],
+                "required_calories": required_calories,
             }
         )
 
