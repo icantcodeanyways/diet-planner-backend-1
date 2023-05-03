6e97ec0e769ce7acdc57f34d88809bf1153fe6ba
Devadathan M B <devadathanmb@gmail.com>
2023-05-03 17:12:54 +0530

    implement get information about user route

--- app/resources/user.py
+++ app/resources/user.py
@@ -1,11 +1,24 @@
 from utils.token_required import token_required
 from flask_restful import Resource
+from bson.objectid import ObjectId
+from db import users
+
 
 class User(Resource):
     # Get user information
     @token_required
     def get(self, user_id):
-        return {"message": "get user info get"}
+        # Find the user
+        user = users.find_one({"_id": ObjectId(user_id)})
+        response = {
+            "first_name": user["first_name"],
+            "last_name": user["last_name"],
+            "dob": user["dob"],
+            "weight": user["weight"],
+            "height": user["height"],
+            "gender": user["gender"],
+        }
+        return response
 
     # Update user info
     @token_required
