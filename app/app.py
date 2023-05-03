9a548838bf5d80fc601e4c6c62ad43e145ae815f
Devadathan M B <devadathanmb@gmail.com>
2023-05-03 15:55:27 +0530

    Protect routes

--- app/app.py
+++ app/app.py
@@ -126,20 +126,24 @@ class UserRegistration(Resource):
 
 class User(Resource):
     # Get user information
+    @token_required
     def get(self, user_id):
         return {"message": "get user info get"}
 
     # Update user info
+    @token_required
     def patch(self, user_id):
         return {"message": "update user info put"}
 
     # Delete user
+    @token_required
     def delete(self, user_id):
         return {"message": "delete user info delete"}
 
 
 class GenerateDietPlan(Resource):
     # Generate diet plan
+    @token_required
     def post(self):
         return {"message": "generate diet plan"}
 
