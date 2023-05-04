52fb97b7f94d54c2ccb2cc9b874e903629d7aebb
Devadathan M B <devadathanmb@gmail.com>
2023-05-04 12:45:56 +0530

    Implement endpoint to retrieve generated diet plans

--- app/resources/diet_plan.py
+++ app/resources/diet_plan.py
@@ -1,10 +1,17 @@
 from flask_restful import Resource
 from utils.token_required import token_required
+from db import users
+from bson.objectid import ObjectId
 
 class DietPlan(Resource):
     # Return geenrated diet plans
     @token_required
-    def get(self):
-        return {"message": "Generated diet plans"}
-
-
+    def get(self, user_id):
+        # Find the user
+        user = users.find_one({"_id" : ObjectId(user_id)})
+        if not user:
+            return {"message" : "User not found"}, 404
+        generated_diet_plans = user["generated_diet_plans"]
+        if len(generated_diet_plans) == 0:
+            return {"message" : "You have no generated meal plans"}, 404
+        return generated_diet_plans, 200
