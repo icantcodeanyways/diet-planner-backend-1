421b1710dd7dbd71e688cc534f7097639fac9fd1
Devadathan M B <devadathanmb@gmail.com>
2023-05-23 00:07:21 +0530

    return more data in GET /api/users/<user_id> endpoint

--- app/resources/user.py
+++ app/resources/user.py
@@ -12,14 +12,47 @@ class User(Resource):
         user = users.find_one({"_id": ObjectId(user_id)})
         if not user:
             return {"message": "User not found"}, 404
+        if len(user["generated_diet_plans"]) == 0:
+            consumed_calories = 0
+            consumed_fat = 0
+            consumed_protien = 0
+            consumed_carbs = 0
+            breakfast = {
+                "consumed_carbs": 0,
+                "consumed_fat": 0,
+                "consumed_protien": 0,
+            }
+            dinner = {
+                "consumed_carbs": 0,
+                "consumed_fat": 0,
+                "consumed_protien": 0,
+            }
+            lunch = {
+                "consumed_carbs": 0,
+                "consumed_fat": 0,
+                "consumed_protien": 0,
+            }
+
         response = {
             "first_name": user["first_name"],
             "last_name": user["last_name"],
+            "activity_factor": user["activity_factor"],
             "dob": user["dob"],
             "weight": user["weight"],
             "height": user["height"],
             "gender": user["gender"],
+            "diet_goal": user["diet_goal"],
             "required_calories": user["required_calories"],
+            "required_carbs": user["required_carbs"],
+            "required_protien": user["required_protien"],
+            "required_fat": user["required_fat"],
+            "consumed_calories": consumed_calories,
+            "consumed_fat": consumed_fat,
+            "consumed_carbs": consumed_carbs,
+            "consumed_protien": consumed_protien,
+            "breakfast_stats": breakfast,
+            "lunch_stats": lunch,
+            "dinner_stats": dinner,
         }
         return response
 
@@ -30,7 +63,13 @@ class User(Resource):
         parser.add_argument("dob", type=str, help="Date of birth cannot be empty")
         parser.add_argument("weight", type=int)
         parser.add_argument("height", type=int)
+        parser.add_argument(
+            "activity_factor", type=float, choices=(1, 1.2, 1.4, 1.6, 1.8)
+        )
+        parser.add_argument("diet_goal", type=str, choices=("gain", "lose", "maintain"))
+
         args = parser.parse_args()
+        print(args)
 
         user = users.find_one({"_id": ObjectId(user_id)})
         if not user:
