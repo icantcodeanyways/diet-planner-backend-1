bb1bb446f2b17218e0e2f1958f4bad0726ef8880
Devadathan M B <devadathanmb@gmail.com>
2023-05-24 01:57:17 +0530

    add edamam api configs

--- app/config.py
+++ app/config.py
@@ -1,2 +1,5 @@
 MONGO_URI = "mongodb+srv://devadathanmb:yyOGRJvtSpJ5PUAu@cluster0.q3u0dpi.mongodb.net/?retryWrites=true&w=majority"
 SECRET_KEY = "NreozOHoKOPHrMMw"
+FOOD_DATABASE_API_ENDPOINT = "https://api.edamam.com/api/food-database/v2/parser"
+APP_ID = "17ef13d8"
+APP_KEY = "1cd19810edca849183d418491db87c92"
